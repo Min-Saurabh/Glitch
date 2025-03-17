c2d73c95be879f137f9a9bd9fe51eac09794cb4b
Saurabh <121770194+Mr-Imposible@users.noreply.github.com>
2025-03-18 00:19:28 +0530

    Structured the Response of Chat Bot

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent,AgentExecutor
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -14,7 +14,7 @@ class Research_Response(BaseModel):
     tools_Used: list[str]
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")  
-parser = PydanticOutputParser(pydantic_object = Research_Response)
+parser = PydanticOutputParser(pydantic_object=Research_Response)
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -22,7 +22,7 @@ prompt = ChatPromptTemplate.from_messages(
             "system",
             """
             You are a research assistant that will help generate a research paper.
-            Answer the user query and use neccessary tools. 
+            Answer the user query and use necessary tools. 
             Wrap the output in this format and provide no other text\n{format_instructions}
             """,
         ),
@@ -38,8 +38,13 @@ agent = create_tool_calling_agent(
     tools=[]
 )
 
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
 
-agent_executor = AgentExecutor(agent = agent,tools=[],verbose = True)
-
-raw_Response = agent_executor.invoke({"query":"What is your name?"})
+raw_Response = agent_executor.invoke({"query": "What is your name?"})
 print(raw_Response)
+
+try:
+    structured_response = parser.parse(raw_Response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_Response)
