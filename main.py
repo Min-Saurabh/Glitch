88ab45d42e49847c55c7a6245de53ca2fcb076d5
Saurabh <121770194+Mr-Imposible@users.noreply.github.com>
2025-03-18 00:02:49 +0530

    Added the Agent

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-<<<<<<< HEAD
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-
-
-load_dotenv()
-
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")  # Use the appropriate Gemini model
-response = llm.invoke("Hello, how are you?")
-print(response)
-=======
-from dotenv import load_dotenv
-from pydantic import BaseModel
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent,AgentExecutor
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")  # Use the appropriate Gemini model
-response = llm.invoke("Hello, how are you?")
-print(response)
->>>>>>> 18eeab31 (Initializes the directory)
+class Research_Response(BaseModel):
+    topic: str
+    response: str
+    sources: list[str]
+    tools_Used: list[str]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")  
+parser = PydanticOutputParser(pydantic_object = Research_Response)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+
+agent_executor = AgentExecutor(agent = agent,tools=[],verbose = True)
+
+raw_Response = agent_executor.invoke({"query":"What is your name?"})
+print(raw_Response)
